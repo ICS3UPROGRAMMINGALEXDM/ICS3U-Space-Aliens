430f3bd1ae6632da2b4b362c181f7fef64f79210
Alexander DeMeo <alexander.demeo@stu.ocsb.ca>
2022-06-09 09:26:49 -0400

    added a background

--- code.py
+++ code.py
@@ -3,5 +3,22 @@
 # Date: 03/25/2022
 # Description: prints a message to the edgebadge
 
-# prints to the edgeBadge
-print("Hello to all my people")
+
+
+
+def game_scene():
+    # his function is the main game scene
+
+    # 3 blank lines
+    print("\n\n\n")
+    # prints to the edgeBadge
+    print("Hello to all my people")
+
+    while True:
+        # repeat forever or intil its turned off
+        pass  # placeholder
+
+
+
+if __name__ == "__main__":
+    game_scene()
