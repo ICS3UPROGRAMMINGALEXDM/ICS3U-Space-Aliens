eb5ba5d7715a47c7a04f151d24a87fa903359529
Alexander DeMeo <alexander.demeo@stu.ocsb.ca>
2022-06-13 09:19:41 -0400

    added a lazer sound

--- code.py
+++ code.py
@@ -3,6 +3,7 @@
 # Date: 03/25/2022
 # Description: This is my CPT game for the edgebadge
 
+
 import stage
 import ugame
 
@@ -13,42 +14,103 @@ def game_scene():
     # his function is the main game scene
 
     # accesses the image bank and setting it to a variable st index 0
-    image_bank_background = stage.Bank.from_bmp16(
-        "space_aliens_background.bmp"
-    )  # sets up a new image bank, this one for the sprites
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+
+    # sets up a new image bank, this one for the sprites
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
+    # This is for the buttons that we want information about their state
+    # 'A button' sets it to the button up state
+    a_button = constants.button_state["button_up"]
+
+    # 'B button' sets is to the button up state
+    b_button = constants.button_state["button_up"]
+
+    # 'Start button' sets it to button up
+    start_button = constants.button_state["button_up"]
+
+    # 'Select button' sets is to button up
+    select_button = constants.button_state["button_up"]
+
+    # The next portion is to set up the sound
+    # this sets a variable to the wav file that holds the sound
+    pew_sound = open("pew.wav", "rb")
+
+    # uses the audio library in ugame
+    sound = ugame.audio
+
+    # makes sure there is no sound in the beginning
+    sound.stop()
+
+    # makes sure that sound isn't muted
+    sound.mute(False)
+
     # creates the 10 by 8 image grid, sets it to background
-    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
-    # creates the ship sprite and sets it to the 5th index of the sprite list
+    background = stage.Grid(
+        image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
+    )
+
+    # creates the ship sprite and sets it to the index 5 of the sprite list
     # puts it to 75,66 on the screen
-    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    ship = stage.Sprite(
+        image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
+    )
+
+    # creates the alien sprite and sets it to index 9 of the sprite list
+    alien = stage.Sprite(
+        image_bank_sprites,
+        9,
+        int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2),
+        16,
+    )
 
     # 60 means 60 hertz which will update it 60 times per second
     game = stage.Stage(ugame.display, constants.FPS)
+
     # accesses the first layer(background) and makes the list of images for the background
-    game.layers = [ship] + [background]
+    game.layers = [ship] + [alien] + [background]
+
     # takes layers and shows them on the screen
     game.render_block()
+
     # this is the game loop so it is supposed to loop forever
     while True:
         # Get user input
         # getting the buttons that are pressed - 60 times a second
         keys = ugame.buttons.get_pressed()
+
         # The if statements decide what to do when a button is pressed
         if keys & ugame.K_X:
             pass
-        if keys & ugame.K_O:
-            pass
+        if keys & ugame.K_O != 0:
+            if a_button == constants.button_state["button_up"]:
+                a_button = constants.button_state["button_just_pressed"]
+            elif a_button == constants.button_state["button_just_pressed"]:
+                a_button = constants.button_state["button_still_pressed"]
+        else:
+            if a_button == constants.button_state["button_still_pressed"]:
+                a_button = constants.button_state["button_released"]
+            else:
+                a_button = constants.button_state["button_up"]
         if keys & ugame.K_START:
             pass
         if keys & ugame.K_SELECT:
             pass
         if keys & ugame.K_RIGHT:
-            # Moves the ship right
-            ship.move(ship.x + 1, ship.y)
+            if ship.x <= constants.SCREEN_X - constants.SPRITE_SIZE:
+                # Moves the ship right
+                ship.move(ship.x + 1, ship.y)
+            else:
+                # stops the ship at the right edge of the screen
+                ship.move(constants.SCREEN_X - constants.SPRITE_SIZE, ship.y)
         if keys & ugame.K_LEFT:
-            # moves the ship left
-            ship.move(ship.x - 1, ship.y)
+            # Checks to ensure the ship is still on screen
+            if ship.x >= 0:
+                # moves the ship left
+                ship.move(ship.x - 1, ship.y)
+            else:
+                # Stops at the left edge of the screen
+                ship.move(0, ship.y)
         if keys & ugame.K_UP:
             # moves the ship up
             pass
@@ -58,9 +120,14 @@ def game_scene():
 
         # update game logic
 
+        # play the pew sound if the A button was just pressed ( in the button_just_pressed state)
+        if a_button == constants.button_state["button_just_pressed"]:
+            sound.play(pew_sound)
+
         # redraw sprites
         # refreshes the ship sprite
-        game.render_sprites([ship])
+        game.render_sprites([ship] + [alien])
+
         # wait until the specified 60th of a second is reached
         game.tick()
 
